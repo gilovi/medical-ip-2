c55a03dd10aecdda0acdacf191c214e6f337dfe4
gil <gilovi@users.noreply.github.com>
2015-01-14 15:47:12 +0200

    shift works

--- Find_Shift.m
+++ Find_Shift.m
@@ -12,7 +12,8 @@ direction = zeros(1,3 );
 direction(dimNum) = 1;
 
 padsize = direction * (hlf+1);
-padarray(image1, padsize);
+image1 = padarray(image1, padsize);
+image2 = padarray(image2, padsize);
 
 curr = 0;
 dist = S;
@@ -37,53 +38,40 @@ while true
     end
     
     di = [l,c,r; curr-dist , curr , curr+dist];
-    %di(1,di(2,:) > S) = -inf; 
     [~,m] = max(di,[],2);
-    
-    %
-    di%
-    %
-    
+   
     if m ~= 2
         c = di(1,m(1));
         curr = di(2,m(1));
-    %else
-        %lst = curr - ceil(abs(curr-lst)/2);
-        
+
     end
     dist = ceil(dist/2);
 end
 
 % %fine tunning
-% sft = ones(1,21)*-inf;
-% for i=1:length(sft)
-%    sft(i) = method(circshift(image1, curr-(i-round(length(sft)/2))),image2);
-% end
-
-
-parfor i= 1:size(image1,dimNum)
-     sft(i) = method(circshift(image1,(direction*i)-hlf),image2) ; 
-     if mod(i,50)== 0
-        figure ; imshowpair(circshift(image1,(direction*i)-hlf),image2) 
-     end
+sft = ones(1,21)*-inf;
+for i=1:length(sft)
+   sft(i) = method(circshift(image1, curr-(i-round(length(sft)/2))),image2);
 end
+
 [~,shift] = max(sft);
 shift = curr-(shift-round(length(sft)/2));
-plot (-hlf:hlf,sft)
 
-% im1 = im2double(image1(:));
-% im2 = im2double(image2(:));
-% 
-% avr1 = mean2(im1);
-% avr2 = mean2(im2);
-% 
-% 
-% f = im1 - avr1;
-% g = im2 - avr2;
-% 
-% ifft2(fft2(f)*conj(fft2(g)))
-% 
-% 
-% grade = (im1 - avr1)'*(im2 - avr2)  / (((sum((im1(:)-avr1).^2)).^(1/2)) * ((sum((im2(:)-avr2).^2)).^(1/2)) );
+%plot (sft)
+
+% % brute force all. slow. for understanding porpuses. 
+% y=zeros (1,size(image1,dimNum));
+% sft=y;
+% for i= 1:size(image1,dimNum)
+%      sft(i) = method(circshift(image1,i-S-1,dimNum),image2) ; 
+%       if mod(i,120)== 0
+%          %figure ; imshowpair(circshift(image1,i-S-1,dimNum),image2) 
+%          y(i)= sft(i);
+%       end
+% end
+% [~,shift] = max(sft);
+% shift = shift-S-1;
+% plot (sft);hold on ;plot(y) ;hold off;
+
 end
 
